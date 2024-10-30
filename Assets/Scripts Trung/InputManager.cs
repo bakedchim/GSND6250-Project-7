7d3838e98ff15289a17178b3cdf0ee89bb5c376a
bakedchim <143598844+bakedchim@users.noreply.github.com>
2024-10-29 18:03:27 -0700

    Add basic input

---
+++ Assets/Scripts Trung/InputManager.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private static InputManager instance;
+
+    public static InputManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = FindObjectOfType<InputManager>();
+            }
+            return instance;
+        }
+    }
+
+    private PlayerControls playerControls;
+    
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+        playerControls = new PlayerControls();
+    }
+
+    void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    void OnDisable()
+    {
+        playerControls.Disable();
+    }
+
+    public Vector2 GetMovementInput()
+    {
+        return playerControls.Player.Move.ReadValue<Vector2>();
+    }
+
+    public Vector2 GetLookInput()
+    {
+        return playerControls.Player.Look.ReadValue<Vector2>();
+    }
+
+    public bool GetJumpInput()
+    {
+        return playerControls.Player.Jump.triggered;
+    }
+}
